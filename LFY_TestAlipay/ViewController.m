ded80ceaf37783fbff316cf9d6c82228246877cc
luffy <lushihong2012@163.com>
2017-03-22 20:59:04 +0800

    测试芝麻认证
    设置白名单

--- LFY_TestAlipay/ViewController.m
+++ LFY_TestAlipay/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self doVerify:@"www.baidu.com"];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +24,33 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)doVerify:(NSString *)url {
+    NSString *alipayUrl = [NSString stringWithFormat:@"alipays://platformapi/startapp?appId=20000067&url=%@", [self URLEncodedStringWithUrl:url]];
+    if ([self canOpenAlipay]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:alipayUrl] options:@{} completionHandler:nil];
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"" message:@"是否下载并安装支付宝完成认证?" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"好的", nil];
+        [alertView show];
+    }
+}
+
+-(NSString *)URLEncodedStringWithUrl:(NSString *)url {
+    NSString *encodedString = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(CFStringRef)url,NULL,(CFStringRef) @"!*'();:@&=+$,%#[]|",kCFStringEncodingUTF8));
+    return encodedString;
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        NSString *appstoreUrl = @"itms-apps://itunes.apple.com/app/id333206289";
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:appstoreUrl] options:@{} completionHandler:nil];
+    }
+}
+
+- (BOOL)canOpenAlipay {
+    return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"alipays://"]];
 }
 
 
